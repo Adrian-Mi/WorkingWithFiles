55e8e2fcff5b56c2d7205acd2774024b7d66fe43
Adrian-Mi <120776299+Adrian-Mi@users.noreply.github.com>
2022-12-17 14:51:27 +0330

    Add files via upload

---
+++ WinFrmFile1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WinFrmFile1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExist = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtDelete = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDest = new System.Windows.Forms.TextBox();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnAppendAllText = new System.Windows.Forms.Button();
+            this.btnAppend = new System.Windows.Forms.Button();
+            this.btnReadAndWrite = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnCreatText = new System.Windows.Forms.Button();
+            this.btnOpenWrite = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnExist);
+            this.panel1.Controls.Add(this.txtPath);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 48);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExist
+            // 
+            this.btnExist.Location = new System.Drawing.Point(3, 14);
+            this.btnExist.Name = "btnExist";
+            this.btnExist.Size = new System.Drawing.Size(98, 23);
+            this.btnExist.TabIndex = 1;
+            this.btnExist.Text = "Is Exist a File?";
+            this.btnExist.UseVisualStyleBackColor = true;
+            this.btnExist.Click += new System.EventHandler(this.btnExist_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(127, 16);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(373, 20);
+            this.txtPath.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.txtDelete);
+            this.panel2.Location = new System.Drawing.Point(12, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(509, 46);
+            this.panel2.TabIndex = 2;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(3, 14);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtDelete
+            // 
+            this.txtDelete.Location = new System.Drawing.Point(127, 16);
+            this.txtDelete.Name = "txtDelete";
+            this.txtDelete.Size = new System.Drawing.Size(373, 20);
+            this.txtDelete.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.txtDest);
+            this.panel3.Controls.Add(this.btnMove);
+            this.panel3.Controls.Add(this.btnCopy);
+            this.panel3.Controls.Add(this.txtSource);
+            this.panel3.Location = new System.Drawing.Point(12, 118);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(509, 105);
+            this.panel3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Destination File";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Source File";
+            // 
+            // txtDest
+            // 
+            this.txtDest.Location = new System.Drawing.Point(100, 47);
+            this.txtDest.Name = "txtDest";
+            this.txtDest.Size = new System.Drawing.Size(400, 20);
+            this.txtDest.TabIndex = 2;
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(115, 73);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(98, 23);
+            this.btnMove.TabIndex = 1;
+            this.btnMove.Text = "Move";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(11, 73);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(98, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(100, 21);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(400, 20);
+            this.txtSource.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.txtDisplay);
+            this.panel4.Controls.Add(this.btnAppendAllText);
+            this.panel4.Controls.Add(this.btnAppend);
+            this.panel4.Controls.Add(this.btnReadAndWrite);
+            this.panel4.Controls.Add(this.btnOpen);
+            this.panel4.Controls.Add(this.btnCreatText);
+            this.panel4.Controls.Add(this.btnOpenWrite);
+            this.panel4.Controls.Add(this.btnCreate);
+            this.panel4.Location = new System.Drawing.Point(12, 229);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(509, 238);
+            this.panel4.TabIndex = 3;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(32, 17);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDisplay.Size = new System.Drawing.Size(443, 110);
+            this.txtDisplay.TabIndex = 2;
+            // 
+            // btnAppendAllText
+            // 
+            this.btnAppendAllText.Location = new System.Drawing.Point(344, 138);
+            this.btnAppendAllText.Name = "btnAppendAllText";
+            this.btnAppendAllText.Size = new System.Drawing.Size(98, 23);
+            this.btnAppendAllText.TabIndex = 1;
+            this.btnAppendAllText.Text = "Append All Text";
+            this.btnAppendAllText.UseVisualStyleBackColor = true;
+            this.btnAppendAllText.Click += new System.EventHandler(this.btnAppendAllText_Click);
+            // 
+            // btnAppend
+            // 
+            this.btnAppend.Location = new System.Drawing.Point(240, 138);
+            this.btnAppend.Name = "btnAppend";
+            this.btnAppend.Size = new System.Drawing.Size(98, 23);
+            this.btnAppend.TabIndex = 1;
+            this.btnAppend.Text = "Append Text";
+            this.btnAppend.UseVisualStyleBackColor = true;
+            this.btnAppend.Click += new System.EventHandler(this.btnAppend_Click);
+            // 
+            // btnReadAndWrite
+            // 
+            this.btnReadAndWrite.Location = new System.Drawing.Point(240, 183);
+            this.btnReadAndWrite.Name = "btnReadAndWrite";
+            this.btnReadAndWrite.Size = new System.Drawing.Size(98, 23);
+            this.btnReadAndWrite.TabIndex = 1;
+            this.btnReadAndWrite.Text = "Read && write";
+            this.btnReadAndWrite.UseVisualStyleBackColor = true;
+            this.btnReadAndWrite.Click += new System.EventHandler(this.btnReadAndWrite_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(136, 183);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(98, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnCreatText
+            // 
+            this.btnCreatText.Location = new System.Drawing.Point(136, 138);
+            this.btnCreatText.Name = "btnCreatText";
+            this.btnCreatText.Size = new System.Drawing.Size(98, 23);
+            this.btnCreatText.TabIndex = 1;
+            this.btnCreatText.Text = "Create Text";
+            this.btnCreatText.UseVisualStyleBackColor = true;
+            this.btnCreatText.Click += new System.EventHandler(this.btnCreatText_Click);
+            // 
+            // btnOpenWrite
+            // 
+            this.btnOpenWrite.Location = new System.Drawing.Point(32, 183);
+            this.btnOpenWrite.Name = "btnOpenWrite";
+            this.btnOpenWrite.Size = new System.Drawing.Size(98, 23);
+            this.btnOpenWrite.TabIndex = 1;
+            this.btnOpenWrite.Text = "Open Write";
+            this.btnOpenWrite.UseVisualStyleBackColor = true;
+            this.btnOpenWrite.Click += new System.EventHandler(this.btnOpenWrite_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(32, 138);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(98, 23);
+            this.btnCreate.TabIndex = 1;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 479);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnExist;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtDelete;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDest;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnCreatText;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnAppend;
+        private System.Windows.Forms.Button btnAppendAllText;
+        private System.Windows.Forms.Button btnOpenWrite;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnReadAndWrite;
+    }
+}
+
---
+++ WinFrmFile1/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinFrmFile1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExist_Click(object sender, EventArgs e)
+        {
+            //FileInfo fi = new FileInfo("path");
+            //if(fi.Exists())
+            //{
+
+            //}
+            if(File.Exists(txtPath.Text))
+            {
+                MessageBox.Show("File exist.");
+            }
+            else
+            {
+                MessageBox.Show("File not exist.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtDelete.Text))
+            {
+                File.Delete(txtDelete.Text);
+                txtDelete.Clear();
+                txtDelete.Focus();
+                MessageBox.Show("file deleted.");
+            }
+            else
+            {
+                MessageBox.Show("File not exist.");
+            }
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtSource.Text))
+            {
+                File.Copy(txtSource.Text,txtDest.Text);
+                txtSource.Clear();
+                txtDest.Clear();
+                txtSource.Focus();
+                MessageBox.Show("file copied.");
+            }
+            else
+            {
+                MessageBox.Show("Source File not exist.");
+            }
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(txtSource.Text))
+            {
+                File.Move(txtSource.Text, txtDest.Text);
+                txtSource.Clear();
+                txtDest.Clear();
+                txtSource.Focus();
+                MessageBox.Show("file Moved.");
+            }
+            else
+            {
+                MessageBox.Show("Source File not exist.");
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            string path = @"c:\myTemp\MyTest.txt";
+
+            try
+            {
+                // Create the file, or overwrite if the file exists.
+                using (FileStream fs = File.Create(path))
+                {
+                    byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file."+ Environment.NewLine);
+                    // Add some information to the file.
+                    fs.Write(info, 0, info.Length);
+                  //  byte[] info = Encoding.ASCII.GetBytes("This is second line.\n"); 
+                    info = Encoding.ASCII.GetBytes("This is second line.\n");
+                    fs.Write(info, 0, info.Length);
+                }
+
+                // Open the stream and read it back.
+               // StreamReader sr = new StreamReader(path);
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        txtDisplay.AppendText(s + Environment.NewLine);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnCreatText_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            string path = @"c:\myTemp\MyTest1.txt";
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    txtDisplay.AppendText(s + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnAppend_Click(object sender, EventArgs e)
+        {
+            //File.AppendText(String) Method
+            //Creates a StreamWriter that appends UTF-8 encoded text to an existing file, 
+            //or to a new file if the specified file does not exist.
+            txtDisplay.Clear();
+            string path = @"c:\myTemp\MyTest2.txt";
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("This");
+                sw.WriteLine("is Extra");
+                sw.WriteLine("Text");
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    txtDisplay.AppendText(s + Environment.NewLine);
+                }
+            }
+        }
+
+        private void btnAppendAllText_Click(object sender, EventArgs e)
+        {
+            //File.AppendAllText Method
+            //Appends the specified string to the file,
+            //creating the file if it does not already exist.
+
+            //WriteAllText(String, String)
+            //Creates a new file, writes the specified string to the file, and then closes the file.
+            //If the target file already exists, it is overwritten.
+
+            //File.ReadAllText Method
+            //Opens a text file, reads all the text in the file into a string,
+            //and then closes the file.
+
+            txtDisplay.Clear();
+            string path = @"c:\myTemp\MyTest3.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = "Hello and Welcome" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            string appendText = "This is extra text" + Environment.NewLine;
+            File.AppendAllText(path, appendText);
+
+            // Open the file to read from.
+            string readText = File.ReadAllText(path);
+            txtDisplay.AppendText(readText + "\n");
+        }
+
+        private void btnOpenWrite_Click(object sender, EventArgs e)
+        {
+            //File.OpenWrite(String) Method
+            //Opens an existing file or creates a new file for writing.
+
+            //File.OpenRead(String) Method
+            //Opens an existing file for reading.
+
+            txtDisplay.Clear();
+            string path = @"c:\myTemp\MyTest4.txt";
+            
+
+            // Open the stream and write to it.
+            using (FileStream fs = File.OpenWrite(path))
+            {
+                Byte[] info =
+                    new UTF8Encoding(true).GetBytes("This is to test the OpenWrite method.");
+
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            // Open the stream and read it back.
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    txtDisplay.AppendText(temp.GetString(b));
+                }
+            }
+            //Additional tips:
+            string result = Path.GetTempPath();
+            MessageBox.Show(result);
+            string[] paths = { @"d:\archives", "2001", "media", "images" };
+            string fullPath = Path.Combine(paths);
+            MessageBox.Show(fullPath);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            //File.Open Method  ---> Open(String, FileMode, FileAccess)
+            //Opens a FileStream on the specified path,
+            //with the specified mode and access with no sharing.
+            // Create a temporary file, and put some data into it.
+
+            txtDisplay.Clear();
+            string path = Path.GetTempFileName();
+            //Path.GetTempFileName Method
+            //Creates a uniquely named, zero-byte temporary file on disk and
+            //returns the full path of that file.
+
+            //Path.GetTempPath Method
+            //Returns the path of the current user's temporary folder.
+            //For Example:
+            //string result = Path.GetTempPath();
+            //MessageBox.Show(result);
+
+            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Write, FileShare.None))
+            {
+                Byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            // Open the stream and read it back.
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    txtDisplay.AppendText(temp.GetString(b));
+                }
+            }
+            MessageBox.Show(path);
+        }
+
+        private void btnReadAndWrite_Click(object sender, EventArgs e)
+        {
+            string srcPath = @"C:\source\SolarSystem.PNG";
+            string destPath = @"C:\myTemp\SolarSystem.PNG";
+            byte[] buff = File.ReadAllBytes(srcPath);            
+            File.WriteAllBytes(destPath, buff);
+
+            string output = "Length of file:" + buff.Length + "\n";
+            output += "The read data was written from the source file to the destination file.";
+            MessageBox.Show(output);
+        }
+    }
+}
+/*
+Check other methods
+GetCreationTime:     Returns a DateTime object representing when the file/directory was created.
+GetLastAccessTime:   Returns a DateTime object representing when the file/directory was last accessed.
+GetLastWriteTime:    Returns a DateTime object representing when the file/directory was last modified.
+*/
